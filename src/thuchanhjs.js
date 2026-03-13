51faf2e65c45b7050384d321660939c6f2bd76ae
DoVu <dkv1412@gmail.com>
2026-03-13 13:19:06 +0700

    thuchanharea

--- src/thuchanhjs.js
+++ src/thuchanhjs.js
@@ -1,36 +1,12 @@
-let number = 10;
+let inputWidth;
+let inputHeight;
 
-let float = 20.5;
-let boolean = true;
-let string = 'Hà Nội';
+inputWidth = prompt("Nhập chiều rộng: ");
+inputHeight = prompt("Nhập chiều dài: ");
 
-document.write('Number = ' + number);
-document.write('<br/>');
-document.write('Float = ' + float);
-document.write('<br/>');
-document.write('Boolean = ' + boolean);
-document.write('<br/>');
-document.write('String = ' + string);
-document.write('<br/>');
-
-let width = 20;
-
-let height = 10;
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
 
 let area = width * height;
 
-document.write("Area: " + area);
-
-function testNumer() {
-    let a = prompt("Nhập số a: ");
-    let b = prompt("Nhập số b: ");
-
-    a = Number(a);
-    b = Number(b);
-
-    if (a % b === 0) {
-        alert(a + " là bội số của " + b);
-    } else {
-        alert(a + " không phải là bội số của " + b);
-    }
-}
+document.write("Diên tích hình chữ nhật là: " + area);
