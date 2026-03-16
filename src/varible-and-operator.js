b0d21b7e593a7f6b8806068310012175cae93ecb
DoVu <dkv1412@gmail.com>
2026-03-16 15:03:56 +0700

    add code

---
+++ src/varible-and-operator.js
@@ -0,0 +1,27 @@
+// let initWithLet;
+// const initWithConst = 1;
+//
+// var initWithVar;
+
+// let x = 5;
+//
+// // y1 = --x;
+// y2 = x--;
+
+// console.log(y2);
+// console.log(y2);
+//
+// let x = 99;
+//
+// let y = x % 5;
+//
+// console.log(y);
+
+let x = 3;
+
+let i = 0;
+while (i < 3) {
+    x += 1;
+    i += 1;
+}
+console.log(x);
