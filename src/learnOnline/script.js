6c2f2962246af3b9017c150bde00ba6d57b342c0
DoVu <dkv1412@gmail.com>
2026-03-18 14:15:17 +0700

    learnOnline

---
+++ src/learnOnline/script.js
@@ -0,0 +1,21 @@
+// console.log("While");
+//
+// var i = 0;
+// while (i < 2) {
+//     console.log(i);
+//     i++;
+// }
+//
+// console.log("End while");
+// console.log(i);
+// console.log("----------------------");
+// console.log("do-while");
+//
+// i = 0;
+// do {
+//     console.log(i);
+//     i++;
+// } while (i < 0);
+//
+// console.log("End do - while");
+// console.log(i);
