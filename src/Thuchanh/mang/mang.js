d72d99fd3cc802a5e93e3d22261a8f6f530db5fb
DoVu <dkv1412@gmail.com>
2026-03-27 09:37:04 +0700

    thuchanhmang2

--- src/Thuchanh/mang/mang.js
+++ src/Thuchanh/mang/mang.js
@@ -1,19 +1,35 @@
-let x = 0;
-let array = Array();
+// Thực hành 1
+// let x = 0;
+// let array = Array();
+//
+// function add_element_to_array() {
+//     array[x] = document.getElementById("txtValue").value;
+//     alert("Element: " + array[x] + " Added at index " + x);
+//     x++;
+//
+//     document.getElementById("txtValue").value = "";
+// }
+//
+// function display_array() {
+//     let e = "<hr/>";
+//     for (let i = 0; i < array.length; i++) {
+//         e += "Element: " + i + " = " + array[i] + "<br/>";
+//     }
+//
+//     document.getElementById("result").innerHTML = e;
+// }
 
-function add_element_to_array() {
-    array[x] = document.getElementById("txtValue").value;
-    alert("Element: " + array[x] + " Added at index " + x);
-    x++;
+// Thực hành 2
+let x = [-3, 5, 1, 3, 2, 10];
 
-    document.getElementById("txtValue").value = "";
+let first = 0;
+let last = x.length - 1;
+
+while (first < last) {
+    let b = x[first];
+    x[first] = x[last];
+    x[last] = b;
+    first++;
+    last--;
 }
-
-function display_array() {
-    let e = "<hr/>";
-    for (let i = 0; i < array.length; i++) {
-        e += "Element: " + i + " = " + array[i] + "<br/>";
-    }
-
-    document.getElementById("result").innerHTML = e;
-}
+document.write(x);
