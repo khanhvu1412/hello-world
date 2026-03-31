82764468a3ddfb3ee77aab1613db39a8b05e0eff
DoVu <dkv1412@gmail.com>
2026-03-31 16:38:58 +0700

    kiemtratudoixung

--- src/Baitap/ham/script.js
+++ src/Baitap/ham/script.js
@@ -13,29 +13,44 @@
 // console.log(isPrime(3))
 
 // Bài 2
-function doi() {
-    let feetValue = Number(document.getElementById("feetValue").value);
-    let meterValue = Number(document.getElementById("meterValue").value);
+// function doi() {
+//     let feetValue = Number(document.getElementById("feetValue").value);
+//     let meterValue = Number(document.getElementById("meterValue").value);
+//
+//     function feetToMeter(feet) {
+//         return 0.305 * feet;
+//     }
+//
+//     function meterToFeet(meter) {
+//         return 3.279 * meter;
+//     }
+//
+//     let result = "";
+//
+//     if (feetValue || meterValue) {
+//         let meter = feetToMeter(feetValue);
+//         let feet = meterToFeet(meterValue);
+//
+//         result = feetValue + " feet = " + meter.toFixed(2) + " meter" + "<br/>" +
+//             meterValue + " meter = " + feet.toFixed(2) + " feet";
+//     } else {
+//         result = "Vui lòng nhập giá trị.";
+//     }
+//
+//     document.getElementById("result").innerHTML = result;
+// }
 
-    function feetToMeter(feet) {
-        return 0.305 * feet;
+// Bài 3 Kiểm tra từ đối xứng
+function isPalindrome(s) {
+    if (s.length <= 1) {
+        return true;
     }
 
-    function meterToFeet(meter) {
-        return 3.279 * meter;
+    if (s[0] !== s[s.length - 1]) {
+        return false;
     }
 
-    let result = "";
-
-    if (feetValue || meterValue) {
-        let meter = feetToMeter(feetValue);
-        let feet = meterToFeet(meterValue);
-
-        result = feetValue + " feet = " + meter.toFixed(2) + " meter" + "<br/>" +
-            meterValue + " meter = " + feet.toFixed(2) + " feet";
-    } else {
-        result = "Vui lòng nhập giá trị.";
-    }
-
-    document.getElementById("result").innerHTML = result;
+    return isPalindrome(s.slice(1, s.length - 1));
 }
+
+console.log(isPalindrome("haah"))
