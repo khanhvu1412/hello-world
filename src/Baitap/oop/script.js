31b4cab1aeb3e7e0d312803dc3aeb3f5227e5468
DoVu <dkv1412@gmail.com>
2026-04-08 09:52:59 +0700

    Bài tập 2 OOP

--- src/Baitap/oop/script.js
+++ src/Baitap/oop/script.js
@@ -1,63 +1,86 @@
 // Bài tập 1
-class Rectangle {
-    width;
-    height;
+// class Rectangle {
+//     width;
+//     height;
+//
+//     constructor(width, height) {
+//         this.width = width;
+//         this.height = height;
+//     }
+//
+//     getWidth() {
+//         return this.width;
+//     }
+//
+//     getHeight() {
+//         return this.height;
+//     }
+//
+//     setWidth(width) {
+//         this.width = width;
+//     }
+//
+//     setHeight(height) {
+//         this.height = height;
+//     }
+//
+//     getArea() {
+//         return this.width * this.height;
+//     }
+//
+//     getPerimeter() {
+//         return (this.width + this.height) * 2;
+//     }
+//
+//     draw(ctx, x = 50, y = 50) {
+//         ctx.beginPath();
+//         ctx.rect(x, y, this.width, this.height);
+//         ctx.fillStyle = "#" + Math.floor(Math.random() * 16777215).toString(16);
+//         ctx.fill();
+//         ctx.stroke();
+//     }
+//
+//     setSize(width, height) {
+//         this.width = width;
+//         this.height = height;
+//     }
+// }
+//
+// let canvas = document.getElementById("myCanvas");
+// let ctx = canvas.getContext("2d");
+//
+// let myRectangle = new Rectangle(300, 50);
+//
+// myRectangle.draw(ctx);
+//
+// console.log("Diện tích: " + myRectangle.getArea());
+// console.log("Chu vi: " + myRectangle.getPerimeter());
+//
+// myRectangle.setSize(200, 100);
+//
+// ctx.clearRect(0, 0, canvas.width, canvas.height);
+// myRectangle.draw(ctx);
 
-    constructor(width, height) {
-        this.width = width;
-        this.height = height;
+// Bài tập 2
+class Temperature {
+    celsius;
+
+    constructor(celsius) {
+        this.celsius = celsius;
     }
 
-    getWidth() {
-        return this.width;
+    toFahrenheit() {
+        return 9 / 5 * this.celsius + 32;
     }
 
-    getHeight() {
-        return this.height;
-    }
-
-    setWidth(width) {
-        this.width = width;
-    }
-
-    setHeight(height) {
-        this.height = height;
-    }
-
-    getArea() {
-        return this.width * this.height;
-    }
-
-    getPerimeter() {
-        return (this.width + this.height) * 2;
-    }
-
-    draw(ctx, x = 50, y = 50) {
-        ctx.beginPath();
-        ctx.rect(x, y, this.width, this.height);
-        ctx.fillStyle = "#" + Math.floor(Math.random() * 16777215).toString(16);
-        ctx.fill();
-        ctx.stroke();
-    }
-
-    setSize(width, height) {
-        this.width = width;
-        this.height = height;
+    toKelvins() {
+        return this.celsius + 273.15;
     }
 }
 
-let canvas = document.getElementById("myCanvas");
-let ctx = canvas.getContext("2d");
+let temperature = new Temperature(28);
 
-let myRectangle = new Rectangle(300, 50);
+let my_fahrenheit = temperature.toFahrenheit();
+let my_kelvin = temperature.toKelvins();
 
-myRectangle.draw(ctx);
-
-console.log("Diện tích: " + myRectangle.getArea());
-console.log("Chu vi: " + myRectangle.getPerimeter());
-
-myRectangle.setSize(200, 100);
-
-ctx.clearRect(0, 0, canvas.width, canvas.height);
-myRectangle.draw(ctx);
-
+document.getElementById("result").innerHTML = "Nhiệt độ F: " + my_fahrenheit + "<br/>" + "Nhiệt độ K: " + my_kelvin;
